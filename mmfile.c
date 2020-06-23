d5b640e2cf1ea42a7a614152915013ee58698e3a
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-06-23 17:29:22 +0200

    Update mmfile.c

--- mmfile.c
+++ mmfile.c
@@ -8,18 +8,28 @@ int main(int argc, char** argv) {
   MPI_Init(NULL, NULL); 
   FILE* file = fopen ("test.mtx", "r");
   float i = 0;
+  int j=0;
   int numrow=0;
   int numcol=0;
   fscanf (file, "%d", &numrow);
   fscanf (file, "%d", &numcol);
   printf ("Matrix %d x %d\n", numrow, numcol);
   //fscanf (file, "%d", &i);    
-  while (!feof (file))
+  while (true)
     {  
+      j=j+1;
       fscanf (file, "%f", &i);
+      if(feof(file)!=0)
+        break;
       printf ("%f ", i);
+      if (j%3==0)
+      {
+        printf ("\n");
+        j=0;
+      }
       
     }
   fclose (file); 
   MPI_Finalize();
 }
+
