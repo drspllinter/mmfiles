7668653acde8b283b95f75dcdb6e17890ff930e9
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-06-23 17:17:56 +0200

    Update mmfile.c

--- mmfile.c
+++ mmfile.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   // Initialize the MPI environment
   MPI_Init(NULL, NULL); 
   FILE* file = fopen ("test.mtx", "r");
-  int i = 0;
+  float i = 0;
   int numrow=0;
   int numcol=0;
   fscanf (file, "%d", &numrow);
