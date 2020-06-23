2866166e0e522af2d313f280ce116ad27b68344f
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-06-23 17:37:33 +0200

    Update mmfile.c

--- mmfile.c
+++ mmfile.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#define TRUE 1
 int main(int argc, char** argv) {
   // Initialize the MPI environment
   MPI_Init(NULL, NULL); 
   FILE* file = fopen ("test.mtx", "r");
+  
   int i = 0;
   int j=0;
   float f=0;
@@ -16,12 +17,12 @@ int main(int argc, char** argv) {
   fscanf (file, "%d", &numcol);
   printf ("Matrix %d x %d\n", numrow, numcol);
   //fscanf (file, "%d", &i);    
-  while (true)
+  while (TRUE)
     {  
       fscanf (file, "%d", &i);
       fscanf (file, "%d", &j);
       fscanf (file, "%f", &f);
-      printf ("M[ %d , %d ] = %f\n", i, j, k);
+      printf ("M[ %d , %d ] = %f\n", i, j, f);
       if(feof(file)!=0)
         break;
     }
