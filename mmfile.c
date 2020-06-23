64fa08ab7aa65375a783e3a07cb4417d51ca242a
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-06-23 16:09:36 +0200

    Update mmfile.c

--- mmfile.c
+++ mmfile.c
@@ -1,11 +1,15 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-main() {
+#include <math.h>
 
-   FILE *fp;
-   char buff[255];
-
-   fp = fopen("/tmp/test.txt", "r");
-   fscanf(fp, "%s", buff);
-   printf("1 : %s\n", buff );
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  FILE *fp;
+  char buff[255];
+  fp = fopen("/tmp/test.txt", "r");
+  fscanf(fp, "%s", buff);
+  printf("1 : %s\n", buff );
+  MPI_Finalize();
 }
