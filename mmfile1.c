9abd8819db814a7fd3fee830d0064fb0c1440119
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-06-23 17:41:57 +0200

    Create mmfile1.c

---
+++ mmfile1.c
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define TRUE 1
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL); 
+  FILE* file = fopen ("test.mtx", "r");
+  
+  int i = 0;
+  int j=0;
+  float f=0;
+  int numrow=0;
+  int numcol=0;
+  fscanf (file, "%d", &numrow);
+  fscanf (file, "%d", &numcol);
+  printf ("Matrix %d x %d\n", numrow, numcol);
+  //fscanf (file, "%d", &i);    
+  while (TRUE)
+    {  
+      fscanf (file, "%d", &i);
+      if(feof(file)!=0)
+        break;
+      fscanf (file, "%d", &j);
+      fscanf (file, "%f", &f);
+      printf ("M[ %d , %d ] = %f\n", i, j, f);
+    }
+  fclose (file); 
+  MPI_Finalize();
+}
